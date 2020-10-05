a93f22d8964170360401243c9f40a8ffacc872fa
luizjunior8 <71145941+luizjunior8@users.noreply.github.com>
2020-10-05 01:09:02 -0300

    Update Endereco.java

--- src/main/java/com/example/project/Endereco.java
+++ src/main/java/com/example/project/Endereco.java
@@ -48,5 +48,56 @@ public class Endereco {
         return this.cep;
     }
 
+    public static String Spacer = System.lineSeparator();
+
+    public void validar_dados_obrigatorios() {
+        
+        if (Loja.checker(this.getLogradouro()) == false){
+			throw new RuntimeException ("O campo logradouro do endereço é obrigatório");
+        }
+        
+        if (Loja.checker(this.getMunicipio()) == false){
+			throw new RuntimeException ("O campo município do endereço é obrigatório");			
+        }
+
+        if (Loja.checker(this.getEstado()) == false){
+			throw new RuntimeException ("O campo estado do endereço é obrigatório");
+        }
+
+    }
+
+    public String dados_endereco() {
+
+        validar_dados_obrigatorios();
+
+        String _logradouro = this.getLogradouro()+ ", ";
+		
+		String _numero = "0";
+
+		if (this.getNumero() == 0){
+			_numero = "s/n";
+		}else{
+			_numero = String.format ("%d", this.getNumero());
+		}
+
+		String _complemento = (Loja.checker(this.getComplemento()) == false) ? "" : " " + this.getComplemento();
+
+        String _bairro = Loja.checker(this.getBairro()) == false ? "" : this.getBairro() + " - ";
+        
+        String _municipio = this.getMunicipio() + " - ";
+
+        String _cep = "";
+
+		if (Loja.checker(this.getCep()) == true){
+            _cep = "CEP:" + this.getCep();
+        }
+
+        return (_logradouro + _numero + _complemento + Spacer +
+         _bairro + _municipio + this.getEstado() + Spacer +
+         _cep);
+
+
+    }
+ 
 
 }
